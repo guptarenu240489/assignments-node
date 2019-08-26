31ea25ec91dbf39af1603d35d34b893606da5dfe
guptarenu240489 <guptarenu24@gmail.com>
2019-08-26 15:34:54 +0530

    assigment 3 added

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
+
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('./utility/path');
 
 const app = express();
 
-express.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'users.html'));
+});
+
+app.use('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'start.html'));
+});
+
+app.listen(3000);
